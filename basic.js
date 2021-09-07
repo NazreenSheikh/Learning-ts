3b2bfa06a0575317f68b8b79692523b22fa1abbb
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-07 23:38:26 +0530

    initial commit

---
+++ basic.js
@@ -0,0 +1,7 @@
+// Get started
+// to install typescript
+// use command => npm install typescript
+// npm install node-ts ((to compile ts to js)
+//basics of typescript
+var message = 'Hello, World!';
+console.log(message); //run in terminall cmd: tsc basic.ts
