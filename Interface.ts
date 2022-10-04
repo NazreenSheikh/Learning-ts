3069eeb69768f831b736af8837cef451200681eb
Garv nanwani <garvwashere@gmail.com>
2022-10-04 22:47:23 +0530

    added definition for interface

--- Interface.ts
+++ Interface.ts
@@ -1,8 +1,12 @@
 // INTERFACE
+// Interfaces define properties, methods, and events, which are the members of the interface. Interfaces contain only the declaration of the members. It is the responsibility of the deriving class to define the members. It often helps in providing a standard structure that the deriving classes would follow.
+
+
 interface Person {
     firstName: string;
     lastName: string;
 }
+
 function greetings(person: Person): string {
     return `Hello ${person.firstName} ${person.lastName}`
 
@@ -16,13 +20,16 @@ const emp={
 console.log(greetings(emp))
 
 // EXTENDING  INTERFACE
+
 interface Mailable {
     send(email: string): boolean
     queue(email: string): boolean
 }
+
 interface FutureMailable extends Mailable {
     later(email: string, after: number): boolean
 }
+
 class Mail implements FutureMailable {
     later(email: string, after: number): boolean {
         console.log(`Send email to ${email} in ${after} ms.`);
